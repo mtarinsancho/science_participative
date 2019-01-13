44ab0323e429a1d7979b1db817090e809aa70a43
mtarinsancho <46625423+mtarinsancho@users.noreply.github.com>
2019-01-13 11:29:51 +0100

    Third commit

--- appPROJET/models.py
+++ appPROJET/models.py
@@ -5,10 +5,10 @@ class Specie(models.Model):
     Nom_latin = models.TextField(null=True)
     Nom_vern = models.TextField(null=True)
     Date = models.DateTimeField(auto_now_add=True, auto_now=False)
-
+    Observateur = models.TextField(null=True)
 
 
 class Ecosystem(models.Model):
     Name = models.TextField(null=True)
 #Link between the table species and the table ecosystems. related_name -> foreign key.
-    species = models.ManyToManyField(Specie, related_name='ecosystems', blank=True)
+    species = models.ManyToManyField(Specie, related_name='ecosystem', blank=True)
